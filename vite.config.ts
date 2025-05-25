98d7d8bfac3583fa59e527b5f586bb52dbd31759
Artem Ivanov <ivart.dev@gmail.com>
2025-05-25 21:34:58 +0300

    build(post-css): add post css (sprint-1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
-    build: {
-        outDir: 'dist'
-    }
-})
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+});
