995c792a47445ee308cbf0bd3e99336847259c27
Artem Ivanov <ivart.dev@gmail.com>
2025-05-25 23:15:01 +0300

    refactor: add newline chars

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -2,6 +2,6 @@ import rawTemplate from './input.hbs?raw';
 import s from './input.module.css';
 
 export const Input = {
-    Component: rawTemplate,
-    styles: s,
+  Component: rawTemplate,
+  styles: s,
 };
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -2,6 +2,6 @@ import rawTemplate from './home.hbs?raw';
 import s from './home.module.css';
 
 export const HomePage = {
-    Component: rawTemplate,
-    styles: s,
+  Component: rawTemplate,
+  styles: s,
 };
